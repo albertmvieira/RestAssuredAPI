337f78674906f7c07d76b18c6e9abda863c09e82
albertmvieira <albertmvieira@hotmail.com>
2019-10-30 14:40:59 -0300

    include serialization e deserialization using POJO class

--- src/test/java/tests/Oauth2GetCourse.java
+++ src/test/java/tests/Oauth2GetCourse.java
@@ -5,10 +5,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import CourseOAuth2.GetCoursePojo;
+import CourseOAuth2.WebAutomationPojo;
 import CourseOAuth2.resourcesCourseOAuth2;
 import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import utils.reusableMethods;
@@ -16,6 +20,9 @@ import utils.reusableMethods;
 import static io.restassured.RestAssured.given;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class Oauth2GetCourse {
@@ -97,12 +104,52 @@ public class Oauth2GetCourse {
 		
 		RestAssured.baseURI = prop.getProperty("Host_Course");
 		
-		String response = given().queryParam("access_token", getAccessToken()).
-		when().log().all().
-		get(resourcesCourseOAuth2.getCourse()).asString();
+		GetCoursePojo resGetCourse = given().queryParam("access_token", getAccessToken()).
+		expect().defaultParser(Parser.JSON). //realizado Parse para Json pois o response header não formata no content-type json, volta como text por isso é necessário para não se perder 
+		when().
+		get(resourcesCourseOAuth2.getCourse()).as(GetCoursePojo.class);
 		
-		System.out.println(response);
+		String expCourseTitleApi = "SoapUI Webservices testing";
+		String[] expCourseTitleWeb = {"Selenium Webdriver Java", "Cypress", "Protractor"}; 
 		
+		//declarando array list que será usando na 2ª forma de validação
+		ArrayList<String> listCourseTitWeb = new ArrayList<String>();
+		
+		
+		System.out.println("LinkedIn: " + resGetCourse.getLinkedIn());
+		System.out.println("Instructor: " + resGetCourse.getInstructor());
+		System.out.println("Title course: " + resGetCourse.getCourses().getApi().get(1).getCourseTitle());
+		
+		Assert.assertEquals(resGetCourse.getLinkedIn(), "https://www.linkedin.com/in/rahul-shetty-trainer/");
+		Assert.assertEquals(resGetCourse.getCourses().getApi().get(1).getCourseTitle(), expCourseTitleApi);
+		
+		//for para percorrer a lista de array "cursos" e ao encontrar curso, validar o preço.
+		for (int i = 0; i < resGetCourse.getCourses().getApi().size(); i++) {
+			if (resGetCourse.getCourses().getApi().get(i).getCourseTitle().equals(expCourseTitleApi) ) {
+				System.out.println(resGetCourse.getCourses().getApi().get(i).getPrice());
+				Assert.assertEquals(resGetCourse.getCourses().getApi().get(i).getPrice(), "40");				
+			}
+		}
+		
+		//Validando os cursos da lista de webautomation
+		List<WebAutomationPojo> listWebAut = resGetCourse.getCourses().getWebAutomation();
+		for (int j = 0; j < listWebAut.size(); j++) {
+			System.out.println("WebAutomation course: " + listWebAut.get(j).getCourseTitle());
+			
+			//A validação pode ser feita de várias maneiras, 
+			//1º validando curso a curso tendo a mesma posição, tanto no na lista quanto no array de strings
+			Assert.assertEquals(listWebAut.get(j).getCourseTitle(), expCourseTitleWeb[j].trim());
+			
+			//2º inserindo os valores do titulo do campo dentro de um Arraylist e comparar o conteudo das duas listas.
+			listCourseTitWeb.add(listWebAut.get(j).getCourseTitle());		
+		}
+		
+		//Convertendo array simples de String para Arraylist, para facilitar a comparação com o ArrayList de cursos adicionados da API
+		List<String> expectedList =  Arrays.asList(expCourseTitleWeb);
+
+		//Comparando o Array list da API com a lista de cursos experada que foi informada no array de string declarado. pode vir do cucumber
+		Assert.assertTrue(listCourseTitWeb.equals(expectedList));
+
 	}
 
 }
